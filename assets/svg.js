7e82b457b1cde6a223fdadfbab31864ec8b2041c
Tijani Habeeb <79145763+habeeb10@users.noreply.github.com>
2021-07-14 21:48:05 +0100

    feat: added svg and png icon

---
+++ assets/svg.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Path } from "react-native-svg";
+import Svg from "react-native-svg";
+
+export default function Arrowright(props) {
+  return (
+    <Svg width="9" height="14" viewBox="0 0 9 14" fill="none" {...props}>
+      <Path
+        d="M5.94089 7.35517L0.792908 12.5031L2.20712 13.9174L8.76932 7.35517L2.20712 0.792969L0.792908 2.20718L5.94089 7.35517Z"
+        fill="#D6D6D6"
+        fill-opacity="0.3"
+      />
+    </Svg>
+  );
+}
