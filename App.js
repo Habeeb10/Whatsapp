2c1d2176c00bbb804e5e57cf09643933b0208884
Tijani Habeeb <79145763+habeeb10@users.noreply.github.com>
2021-08-20 01:05:00 +0100

    feat: updated app.js

--- App.js
+++ App.js
@@ -1,8 +1,6 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-import Authorization from "./src/screens/authorization/authorization";
+import Navigator from "./src/components/navigator/mainNavigation";
 
 export default function App() {
-  return <Authorization />;
+  return <Navigator />;
 }
